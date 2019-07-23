4a12d8362900439a801d1e9398b866f225d651cf
leehanchung <kein520@yahoo.com>
2019-07-23 00:55:14 -0700

    han first notebook

--- models/linear.py
+++ models/linear.py
@@ -4,25 +4,26 @@ from sklearn.pipeline import Pipeline
 import category_encoders as ce
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_absolute_error
 
 
-def Linear(X, y):
+def linear(X, y):
 
 	lr = LinearRegression()
 	encoder = ce.OrdinalEncoder()
-	scaler = StandardScaler(with_mean=False)
+	#scaler = StandardScaler(with_mean=False)
 
-	pipe = PipeLine(steps = [('encoder', encoder),
-							 ('scaler', scaler),
+	pipe = Pipeline(steps = [('encoder', encoder),
+							 #('scaler', scaler),
 							 ('lr', lr),
 							])
 	
 	param_grid = {}
 	
 	search = GridSearchCV(pipe, param_grid, n_jobs=-1, cv=5)
-	%time search.fit(X, y)
-	print("Training Score (R^2): {}".format(search.best_score))
+	search.fit(X, y)
+	print("Training Score (R^2): {}".format(search.best_score_))
 	print("Best Parameters: {}".format(search.best_params_))
-	print("Training Accuracy: {}".format(search.best_estimator_predict(X, y)))
+	print("Mean absolute error: {}".format(mean_absolute_error(y, search.best_estimator_.predict(X))))
 	
 	return search.best_estimator_

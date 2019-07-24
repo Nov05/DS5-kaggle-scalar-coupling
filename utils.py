8ef0f311911be3ec995e6f70a189e1db74b94b5c
leehanchung <kein520@yahoo.com>
2019-07-24 09:13:11 -0700

    han eda

--- utils.py
+++ utils.py
@@ -4,11 +4,24 @@ import psutil
 import os
 
 
+def group_mean_log_mae(y_true, y_pred, types, floor=1e-9):
+	"""
+	Fast metric computation for this competition: https://www.kaggle.com/c/champs-scalar-coupling
+	Code is from this kernel: https://www.kaggle.com/uberkinder/efficient-metric
+	
+	Han
+	"""
+	maes = (y_true-y_pred).abs().groupby(types).mean()
+	return np.log(maes.map(lambda x: max(x, floor))).mean()
+
+
 def reduce_mem_usage(df, verbose=True):
 	"""
 	This function reduces the numeric to the least possible numeric type that fits the data so 
 	memory usage during transforming and training will be reduced.
 	Taken from: https://www.kaggle.com/todnewman/keras-neural-net-for-champs
+	
+	Han
 
 	Parameters:
 	===========
@@ -53,43 +66,48 @@ def reduce_mem_usage(df, verbose=True):
 def show_ram_usage():
 	"""
 	This function shows current Python memory usage.
+	
+	Han
 	"""
 	py = psutil.Process(os.getpid())
 	print('RAM usage: {} GB'.format(py.memory_info()[0]/2. ** 30))
 	pass
 
 
+
 def merge_train_structures(train, structures):
+	"""
+	This function is used to merge the structures dataset to the 
+	original train dataset
+	
+	Chance
+
+	Parameters:
+	===========
+	train: train dataframe
+	structures: structures dataframe.
+
+	Output:
+	===========
+	dataframe: merged dataframe
+	"""
   
-  """This function is used to merge the structures dataset to the 
-     original train dataset
-     
-     Parameters:
-     ===========
-	   train: train dataframe
-	   structures: structures dataframe.
-     
-     Output:
-	   ===========
-	   dataframe: merged dataframe
-     """
-  
-  structures = structures.rename({'atom_index': 'atom_index_0',
-                                  'x':'x_0', 'y':'y_0', 'z':'z_0',
-                                  'atom':'atom_0'}, axis=1)
-  
-  merged = pd.merge(train, structures, on=['molecule_name', 'atom_index_0'])
-  
-  structures = structures.rename({'atom_index_0': 'atom_index_1',
-                                  'x_0':'x_1', 'y_0':'y_1', 'z_0':'z_1',
-                                  'atom_0':'atom_1'}, axis=1)
-  
-  merged_1 = pd.merge(merged, structures, on=['molecule_name', 'atom_index_1'])
-  
-  structures = structures.rename({'atom_index_1': 'atom_index',
-                                  'x_1':'x', 'y_1':'y', 'z_1':'z',
-                                  'atom_1':'atom'}, axis=1)
-  
-  assert train.shape[0] == merged.shape[0]
-  
-  return merged_1
+	structures = structures.rename({'atom_index': 'atom_index_0',
+								  'x':'x_0', 'y':'y_0', 'z':'z_0',
+								  'atom':'atom_0'}, axis=1)
+
+	merged = pd.merge(train, structures, on=['molecule_name', 'atom_index_0'])
+
+	structures = structures.rename({'atom_index_0': 'atom_index_1',
+								  'x_0':'x_1', 'y_0':'y_1', 'z_0':'z_1',
+								  'atom_0':'atom_1'}, axis=1)
+
+	merged_1 = pd.merge(merged, structures, on=['molecule_name', 'atom_index_1'])
+
+	structures = structures.rename({'atom_index_1': 'atom_index',
+								  'x_1':'x', 'y_1':'y', 'z_1':'z',
+								  'atom_1':'atom'}, axis=1)
+
+	assert train.shape[0] == merged.shape[0]
+
+	return merged_1
